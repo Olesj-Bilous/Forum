01923061b181824fda336d988f3548bcee84ab24
Olesj Bilous <olesjvbilous@gmail.com>
2022-04-04 23:32:55 +0200

    add RatingController and RatingPane js (in site.js)

---
+++ Web/Controllers/RatingController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Data.Entities;
+using Data.Repositories;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    [Route("Rate")]
+    [ApiController]
+    public class RatingController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public RatingController(Context context)
+        {
+            _context = context;
+        }
+
+        // POST: api/Rating
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<IActionResult> PostRating([FromBody] RatingViewModel model)
+        {
+            var rating = await _context.Ratings.FindAsync(model.UserId, model.MessageId);
+            var msg = await _context.Messages.FindAsync(model.MessageId);
+            
+            if (rating is null)
+            {
+                rating = new Rating();
+                rating.UserId = model.UserId;
+                rating.MessageId = model.MessageId;
+                rating.Rate = model.Rate;
+                _context.Ratings.Add(rating);
+                msg.RateSum += model.Rate;
+            }
+            else
+            {
+                if (rating.Rate == model.Rate)
+                {
+                    _context.Ratings.Remove(rating);
+                    msg.RateSum -= model.Rate;
+                }
+                else
+                {
+                    rating.Rate = model.Rate;
+                    _context.Ratings.Attach(rating).State = EntityState.Modified;
+                    msg.RateSum += 2 * model.Rate;
+                }
+            }
+            _context.Messages.Attach(msg).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                throw;
+            }
+            return Ok();
+        }
+
+        // DELETE: api/Rating/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRating(int id)
+        {
+            var rating = await _context.Ratings.FindAsync(id);
+            if (rating == null)
+            {
+                return NotFound();
+            }
+
+            _context.Ratings.Remove(rating);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool RatingExists(int userId, int msgId)
+        {
+            return _context.Ratings.Any(x => x.UserId == userId && x.MessageId == msgId);
+        }
+    }
+}
--- Web/wwwroot/js/site.js
+++ Web/wwwroot/js/site.js
@@ -1,4 +1,33 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
 
-// Write your JavaScript code.
+Array.from(document.querySelectorAll(".fa-thumbs-up, .fa-thumbs-down"))
+    .forEach(x => x
+        .addEventListener("click", Rate));
+
+
+async function Rate(e) {
+    const target = e.currentTarget;
+    const set = target.dataset;
+    const rate = parseInt(set.rate);
+    const data = { "UserId": set.userId, "MessageId": set.msgId, "Rate": rate };
+    const response = await fetch(
+        'http://localhost:5105/Rate',
+        {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data)
+        });
+    const rateSumContainer = rate > 0 ? target.nextElementSibling : target.previousElementSibling;
+    const targetInverse = rate > 0 ? rateSumContainer.nextElementSibling : rateSumContainer.previousElementSibling;
+    if (response.ok) {
+        let rateSum = parseInt(rateSumContainer.textContent);
+        rateSumContainer.textContent = target.classList.contains("rated")
+                ? rateSum - rate
+                : targetInverse.classList.contains("rated")
+                    ? rateSum + 2 * rate
+                    : rateSum + rate;
+        target.classList.toggle("rated");
+        if (targetInverse.classList.contains("rated"))
+            targetInverse.classList.remove("rated");
+    }
+}
