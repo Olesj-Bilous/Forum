8089892e47316cceee5fc3fdb442b3f77c0b0b63
Olesj Bilous <olesjvbilous@gmail.com>
2022-04-04 23:33:19 +0200

    update AccountPane view and add ViewModels

---
+++ Web/Models/RatingPaneViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Data.Entities;
+
+namespace Web.Models
+{
+    public class RatingPaneViewModel
+    {
+        public int UserId { get; set; }
+        public int Rate { get; set; } = 0;
+        public Message Message { get; set; }
+    }
+}
---
+++ Web/Models/RatingViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Web.Models
+{
+    public class RatingViewModel
+    {
+        public int UserId { get; set; }
+        public int MessageId { get; set; }
+        public int Rate { get; set; }
+    }
+}
