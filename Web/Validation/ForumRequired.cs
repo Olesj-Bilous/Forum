7ffa0cc29b3c5235621467c73f092093ab0dca25
Olesj Bilous <olesjvbilous@gmail.com>
2022-04-04 23:30:58 +0200

    add Validation: ForumRequired

---
+++ Web/Validation/ForumRequired.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Validation
+{
+    public class ForumRequired : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is null)
+            {
+                return new ValidationResult($"{validationContext.DisplayName} is required.");
+            }
+            else
+            {
+                return ValidationResult.Success;
+            }
+        }
+    }
+}
