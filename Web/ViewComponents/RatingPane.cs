5bd075881bd0b651462fdb85d9d8d906df22e5f1
Olesj Bilous <olesjvbilous@gmail.com>
2022-04-04 23:30:19 +0200

    RatingPane ViewComponent

---
+++ Web/ViewComponents/RatingPane.cs
@@ -0,0 +1,29 @@
+﻿using Data.Entities;
+using Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Web.Extensions;
+using Web.Models;
+
+namespace Web.ViewComponents
+{
+    public class RatingPane : ViewComponent
+    {
+        private readonly Context _context;
+
+        public RatingPane(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(Message msg)
+        {
+            RatingPaneViewModel model = new();
+            var userId = HttpContext.Session.GetObject<User>("User").Id;
+            var rating = await _context.Ratings.FindAsync(userId, msg.Id);
+            model.UserId = userId;
+            model.Message = msg;
+            model.Rate = rating is null ? 0 : rating.Rate;
+            return View(model);
+        }
+    }
+}
